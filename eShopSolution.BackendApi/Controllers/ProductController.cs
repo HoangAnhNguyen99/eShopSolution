26ed4c9324e9ed902dbe70061b99057b5e9d7e03
HoangAnhNguyen99 <work.time.anhnh@gmail.com>
2022-07-07 17:01:36 +0700

    Add code

--- eShopSolution.BackendApi/Controllers/ProductController.cs
+++ eShopSolution.BackendApi/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using eShopSolution.Application.Catalog.Products;
+using eShopSolution.ViewModels.Catalog.Products;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,15 +10,26 @@ namespace eShopSolution.BackendApi.Controllers
     public class ProductController : ControllerBase
     {
         private readonly IPublicProductService _publicProductService;
-        public ProductController(IPublicProductService publicProductService)
+        private readonly IManageProductService _manageProductService;
+        public ProductController(IPublicProductService publicProductService, IManageProductService manageProductService)
         {
             _publicProductService = publicProductService;
+            _manageProductService = manageProductService;
         }
 
+        //http://localhost:port/product
         [HttpGet]
         public async Task<IActionResult> Get()
         {
             var products = await _publicProductService.GetAll();
+            return Ok(products);
+        }
+
+        //http://localhost:port/product/public-paging
+        [HttpGet("public-paging")]
+        public async Task<IActionResult> Get([FromQuery] GetPublicProductPagingRequest request)
+        {
+            var products = await _publicProductService.GetAllByCategoryId(request);
             return Ok(products);
         }
     }
